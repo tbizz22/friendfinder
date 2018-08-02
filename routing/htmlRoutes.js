94320c3b94c6d8dc457ffd4a5d09af062e430bee
tyler bisbee <tyler.bisbee@gmail.com>
2018-08-02 00:30:48 -0400

    working handlebars

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var exphbs = require("express-handlebars");
 
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var app = express();
+app.engine("handlebars", exphbs({
+    defaultLayout: "/home"
+}));
+app.set("view engine", "handlebars");
 
+
+var friends = require("../app/data/friends.js")
 
 
 // Routing
@@ -12,21 +20,28 @@ var path = require("path");
 
 
 // middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
     next()
-  })
+})
 
 
 //   homepage route
 router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-    // res.send("is this thing on?")
+    // 
+    res.render("hpbody", {
+        layout: "home"
+    })
+
 })
 
 //survey route
 router.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    res.render("questions", {
+        layout: "survey",
+        questions: friends.questions,
+        choices: friends.choices
+    })
 })
 
 
